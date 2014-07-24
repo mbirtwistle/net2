15930b5a43d8863d9514886657d7e396b2a64c8c
netcoin-shrodingerscat <madprofezzor@gmail.com>
2014-07-24 14:33:50 +0100

    POW/POS staking active

--- src/main.h
+++ src/main.h
@@ -72,8 +72,8 @@ static const int BLOCK_HEIGHT_FINALPOW =  1296000; // this is where the proof of
 
 
 static const int BLOCK_HEIGHT_KGW_START_TESTNET = 5;
-static const int BLOCK_HEIGHT_POS_AND_DIGISHIELD_START_TESTNET =120;
-static const int BLOCK_HEIGHT_FINALPOW_TESTNET =  240;
+static const int BLOCK_HEIGHT_POS_AND_DIGISHIELD_START_TESTNET =10;
+static const int BLOCK_HEIGHT_FINALPOW_TESTNET =  10000;
 
 static const uint256 hashGenesisBlockTestNet("0x4a1ed64aed30d471b268b7a3ba634d4c63955700db462093a20e3f1f9db6a13f"); //("0x63141eded213b050e2aca8e6beb2070fa37c3520f1c492fd5a82a03c337e90f3");
 
